e143e035350153cf09bf8a6bda822975cbff1f87
jayarasan <jayanthi@upm.edu.my>
2025-07-18 13:41:11 +0800

    Update mle_imp.R

--- R/mle_imp.R
+++ R/mle_imp.R
@@ -16,50 +16,51 @@ mle_imp <- function(left, right, dist = "weibull",
                                "harmonic_median", "geometric_median", "random_survival")) {
   impute <- match.arg(impute)
   prelim_times <- ifelse(is.finite(right), (left + right) / 2, left)
+  prelim_times <- pmax(prelim_times, 1e-5)  # avoid log(0) or negative issues
 
   # --- Initial negative log-likelihood
   neg_loglik_init <- switch(dist,
-                            "weibull" = function(par) -sum(dweibull(prelim_times, par[1], par[2], log = TRUE)),
-                            "lognormal" = function(par) -sum(dlnorm(prelim_times, par[1], par[2], log = TRUE)),
-                            "loglogistic" = function(par) {
-                              shape <- par[1]; scale <- par[2]
-                              logf <- log(shape) - log(scale) + (shape - 1) * log(prelim_times / scale) -
-                                2 * log1p((prelim_times / scale)^shape)
-                              -sum(logf)
-                            },
-                            "logistic" = function(par) -sum(dlogis(prelim_times, par[1], par[2], log = TRUE)),
-                            "normal" = function(par) -sum(dnorm(prelim_times, par[1], par[2], log = TRUE)),
-                            "gamma" = function(par) -sum(dgamma(prelim_times, par[1], par[2], log = TRUE)),
-                            "gompertz" = function(par) {
-                              a <- par[1]; b <- par[2]
-                              if (a <= 0 || b <= 0) return(Inf)
-                              logf <- log(b) + a * prelim_times - (b / a) * (exp(a * prelim_times) - 1)
-                              -sum(logf)
-                            },
-                            "EMV" = function(par) {
-                              loc <- par[1]; scale <- par[2]
-                              if (scale <= 0 || any(prelim_times <= 0)) return(Inf)
-                              z <- (log(prelim_times) - loc) / scale
-                              logf <- -log(prelim_times) - log(scale) - z - exp(-z)
-                              -sum(logf)
-                            },
-                            "exp" = function(par) {
-                              if (par[1] <= 0) return(Inf)
-                              -sum(dexp(prelim_times, rate = 1 / par[1], log = TRUE))
-                            },
-                            stop("Unsupported distribution.")
+    "weibull" = function(par) -sum(dweibull(prelim_times, par[1], par[2], log = TRUE)),
+    "lognormal" = function(par) -sum(dlnorm(prelim_times, par[1], par[2], log = TRUE)),
+    "loglogistic" = function(par) {
+      shape <- par[1]; scale <- par[2]
+      logf <- log(shape) - log(scale) + (shape - 1) * log(prelim_times / scale) -
+              2 * log1p((prelim_times / scale)^shape)
+      -sum(logf)
+    },
+    "logistic" = function(par) -sum(dlogis(prelim_times, par[1], par[2], log = TRUE)),
+    "normal" = function(par) -sum(dnorm(prelim_times, par[1], par[2], log = TRUE)),
+    "gamma" = function(par) -sum(dgamma(prelim_times, par[1], par[2], log = TRUE)),
+    "gompertz" = function(par) {
+      a <- par[1]; b <- par[2]
+      if (a <= 0 || b <= 0) return(Inf)
+      logf <- log(b) + a * prelim_times - (b / a) * (exp(a * prelim_times) - 1)
+      -sum(logf)
+    },
+    "EMV" = function(par) {
+      loc <- par[1]; scale <- par[2]
+      if (scale <= 0 || any(prelim_times <= 0)) return(Inf)
+      z <- (log(prelim_times) - loc) / scale
+      logf <- -log(prelim_times) - log(scale) - z - exp(-z)
+      -sum(logf)
+    },
+    "exp" = function(par) {
+      if (par[1] <= 0) return(Inf)
+      -sum(dexp(prelim_times, rate = 1 / par[1], log = TRUE))
+    },
+    stop("Unsupported distribution.")
   )
 
   init <- switch(dist,
-                 "weibull" = c(1, 1),
-                 "lognormal" = c(mean(log(prelim_times)), sd(log(prelim_times))),
-                 "loglogistic" = c(1, 1),
-                 "logistic" = c(median(prelim_times), sd(prelim_times)),
-                 "normal" = c(mean(prelim_times), sd(prelim_times)),
-                 "gamma" = c(1, 1),
-                 "gompertz" = c(0.1, 0.1),
-                 "EMV" = c(mean(log(prelim_times)), sd(log(prelim_times))),
-                 "exp" = c(mean(prelim_times))
+    "weibull" = c(1, 1),
+    "lognormal" = c(mean(log(prelim_times)), sd(log(prelim_times))),
+    "loglogistic" = c(1, 1),
+    "logistic" = c(median(prelim_times), sd(prelim_times)),
+    "normal" = c(mean(prelim_times), sd(prelim_times)),
+    "gamma" = c(1, 1),
+    "gompertz" = c(0.1, 0.1),
+    "EMV" = c(mean(log(prelim_times)), sd(log(prelim_times))),
+    "exp" = c(mean(prelim_times))
   )
 
   fit0 <- optim(par = init, fn = neg_loglik_init, method = "L-BFGS-B", lower = rep(1e-5, length(init)))
@@ -67,27 +68,27 @@ mle_imp <- function(left, right, dist = "weibull",
 
   # --- CDF and Quantile functions
   cdf_fn <- switch(dist,
-                   "weibull" = function(t) pweibull(t, shape = est_par[1], scale = est_par[2]),
-                   "lognormal" = function(t) plnorm(t, meanlog = est_par[1], sdlog = est_par[2]),
-                   "loglogistic" = function(t) 1 / (1 + (t / est_par[2])^(-est_par[1])),
-                   "logistic" = function(t) plogis(t, location = est_par[1], scale = est_par[2]),
-                   "normal" = function(t) pnorm(t, mean = est_par[1], sd = est_par[2]),
-                   "gamma" = function(t) pgamma(t, shape = est_par[1], scale = est_par[2]),
-                   "gompertz" = function(t) 1 - exp(-(est_par[2] / est_par[1]) * (exp(est_par[1] * t) - 1)),
-                   "EMV" = function(t) 1 - exp(-exp(-(log(t) - est_par[1]) / est_par[2])),
-                   "exp" = function(t) pexp(t, rate = 1 / est_par[1])
+    "weibull" = function(t) pweibull(t, shape = est_par[1], scale = est_par[2]),
+    "lognormal" = function(t) plnorm(t, meanlog = est_par[1], sdlog = est_par[2]),
+    "loglogistic" = function(t) 1 / (1 + (t / est_par[2])^(-est_par[1])),
+    "logistic" = function(t) plogis(t, location = est_par[1], scale = est_par[2]),
+    "normal" = function(t) pnorm(t, mean = est_par[1], sd = est_par[2]),
+    "gamma" = function(t) pgamma(t, shape = est_par[1], scale = est_par[2]),
+    "gompertz" = function(t) 1 - exp(-(est_par[2] / est_par[1]) * (exp(est_par[1] * t) - 1)),
+    "EMV" = function(t) 1 - exp(-exp(-(log(t) - est_par[1]) / est_par[2])),
+    "exp" = function(t) pexp(t, rate = 1 / est_par[1])
   )
 
   quantile_fn <- switch(dist,
-                        "weibull" = function(p) qweibull(p, shape = est_par[1], scale = est_par[2]),
-                        "lognormal" = function(p) qlnorm(p, meanlog = est_par[1], sdlog = est_par[2]),
-                        "loglogistic" = function(p) est_par[2] * ((1 - p) / p)^(-1 / est_par[1]),
-                        "logistic" = function(p) qlogis(p, location = est_par[1], scale = est_par[2]),
-                        "normal" = function(p) qnorm(p, mean = est_par[1], sd = est_par[2]),
-                        "gamma" = function(p) qgamma(p, shape = est_par[1], scale = est_par[2]),
-                        "gompertz" = function(p) log(1 - log(1 - p) * (est_par[1] / est_par[2])) / est_par[1],
-                        "EMV" = function(p) exp(est_par[1] - est_par[2] * log(-log(1 - p))),
-                        "exp" = function(p) qexp(p, rate = 1 / est_par[1])
+    "weibull" = function(p) qweibull(p, shape = est_par[1], scale = est_par[2]),
+    "lognormal" = function(p) qlnorm(p, meanlog = est_par[1], sdlog = est_par[2]),
+    "loglogistic" = function(p) est_par[2] * ((1 - p) / p)^(-1 / est_par[1]),
+    "logistic" = function(p) qlogis(p, location = est_par[1], scale = est_par[2]),
+    "normal" = function(p) qnorm(p, mean = est_par[1], sd = est_par[2]),
+    "gamma" = function(p) qgamma(p, shape = est_par[1], scale = est_par[2]),
+    "gompertz" = function(p) log(1 - log(1 - p) * (est_par[1] / est_par[2])) / est_par[1],
+    "EMV" = function(p) exp(est_par[1] - est_par[2] * log(-log(1 - p))),
+    "exp" = function(p) qexp(p, rate = 1 / est_par[1])
   )
 
   # --- Imputation step
@@ -120,40 +121,42 @@ mle_imp <- function(left, right, dist = "weibull",
       }
     }
   }
+  times <- pmax(times, 1e-5)  # prevent log(0), NaNs, Infs
 
   # --- Final MLE on imputed data
   neg_loglik_final <- switch(dist,
-                             "weibull" = function(par) -sum(dweibull(times, par[1], par[2], log = TRUE)),
-                             "lognormal" = function(par) -sum(dlnorm(times, par[1], par[2], log = TRUE)),
-                             "loglogistic" = function(par) {
-                               shape <- par[1]; scale <- par[2]
-                               logf <- log(shape) - log(scale) + (shape - 1) * log(times / scale) -
-                                 2 * log1p((times / scale)^shape)
-                               -sum(logf)
-                             },
-                             "logistic" = function(par) -sum(dlogis(times, par[1], par[2], log = TRUE)),
-                             "normal" = function(par) -sum(dnorm(times, par[1], par[2], log = TRUE)),
-                             "gamma" = function(par) -sum(dgamma(times, par[1], par[2], log = TRUE)),
-                             "gompertz" = function(par) {
-                               a <- par[1]; b <- par[2]
-                               if (a <= 0 || b <= 0) return(Inf)
-                               logf <- log(b) + a * times - (b / a) * (exp(a * times) - 1)
-                               -sum(logf)
-                             },
-                             "EMV" = function(par) {
-                               loc <- par[1]; scale <- par[2]
-                               if (scale <= 0 || any(times <= 0)) return(Inf)
-                               z <- (log(times) - loc) / scale
-                               logf <- -log(times) - log(scale) - z - exp(-z)
-                               -sum(logf)
-                             },
-                             "exp" = function(par) {
-                               if (par[1] <= 0) return(Inf)
-                               -sum(dexp(times, rate = 1 / par[1], log = TRUE))
-                             }
+    "weibull" = function(par) -sum(dweibull(times, par[1], par[2], log = TRUE)),
+    "lognormal" = function(par) -sum(dlnorm(times, par[1], par[2], log = TRUE)),
+    "loglogistic" = function(par) {
+      shape <- par[1]; scale <- par[2]
+      logf <- log(shape) - log(scale) + (shape - 1) * log(times / scale) -
+              2 * log1p((times / scale)^shape)
+      -sum(logf)
+    },
+    "logistic" = function(par) -sum(dlogis(times, par[1], par[2], log = TRUE)),
+    "normal" = function(par) -sum(dnorm(times, par[1], par[2], log = TRUE)),
+    "gamma" = function(par) -sum(dgamma(times, par[1], par[2], log = TRUE)),
+    "gompertz" = function(par) {
+      a <- par[1]; b <- par[2]
+      if (a <= 0 || b <= 0) return(Inf)
+      logf <- log(b) + a * times - (b / a) * (exp(a * times) - 1)
+      -sum(logf)
+    },
+    "EMV" = function(par) {
+      loc <- par[1]; scale <- par[2]
+      if (scale <= 0 || any(times <= 0)) return(Inf)
+      z <- (log(times) - loc) / scale
+      logf <- -log(times) - log(scale) - z - exp(-z)
+      -sum(logf)
+    },
+    "exp" = function(par) {
+      if (par[1] <= 0) return(Inf)
+      -sum(dexp(times, rate = 1 / par[1], log = TRUE))
+    }
   )
 
-  fit <- optim(par = est_par, fn = neg_loglik_final, method = "L-BFGS-B", lower = rep(1e-5, length(est_par)), hessian = TRUE)
+  fit <- optim(par = est_par, fn = neg_loglik_final, method = "L-BFGS-B",
+               lower = rep(1e-5, length(est_par)), hessian = TRUE)
 
   estimates <- setNames(fit$par, if (dist == "exp") "scale" else c("param1", "param2"))
 
@@ -186,3 +189,4 @@ mle_imp <- function(left, right, dist = "weibull",
     converged = fit$convergence == 0
   ))
 }
+
