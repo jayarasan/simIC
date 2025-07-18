cf2426000ba8355bf93e23f78067604ee181f3d9
jayarasan <jayanthi@upm.edu.my>
2025-07-18 14:16:08 +0800

    Update mle_imp.R

--- R/mle_imp.R
+++ R/mle_imp.R
@@ -6,7 +6,6 @@
 #' @param right Right bounds of censoring intervals
 #' @param dist Distribution name (e.g. "weibull", "loglogistic", "EMV")
 #' @param impute Imputation method: "midpoint", "random", etc.
-#'
 #' @return A list containing estimates, standard errors, and log-likelihood
 #' @export
 
@@ -29,7 +28,10 @@ mle_imp <- function(left, right, dist = "weibull",
     },
     "logistic" = function(par) -sum(dlogis(prelim_times, par[1], par[2], log = TRUE)),
     "normal" = function(par) -sum(dnorm(prelim_times, par[1], par[2], log = TRUE)),
-    "gamma" = function(par) -sum(dgamma(prelim_times, par[1], par[2], log = TRUE)),
+    "gamma" = function(par) {
+      if (any(par <= 0)) return(Inf)
+      -sum(dgamma(prelim_times, par[1], par[2], log = TRUE))
+    },
     "gompertz" = function(par) {
       a <- par[1]; b <- par[2]
       if (a <= 0 || b <= 0) return(Inf)
@@ -85,7 +87,11 @@ mle_imp <- function(left, right, dist = "weibull",
     "logistic" = function(p) qlogis(p, location = est_par[1], scale = est_par[2]),
     "normal" = function(p) qnorm(p, mean = est_par[1], sd = est_par[2]),
     "gamma" = function(p) qgamma(p, shape = est_par[1], scale = est_par[2]),
-    "gompertz" = function(p) log(1 - log(1 - p) * (est_par[1] / est_par[2])) / est_par[1],
+    "gompertz" = function(p) {
+      val <- tryCatch(log(1 - log(1 - p) * (est_par[1] / est_par[2])) / est_par[1],
+                      error = function(e) NA)
+      ifelse(is.finite(val) & val > 0, val, NA)
+    },
     "EMV" = function(p) exp(est_par[1] - est_par[2] * log(-log(1 - p))),
     "exp" = function(p) qexp(p, rate = 1 / est_par[1])
   )
@@ -100,14 +106,11 @@ mle_imp <- function(left, right, dist = "weibull",
     } else if (impute == "random") {
       times[i] <- runif(1, left[i], right[i])
     } else if (impute == "median") {
-      x <- runif(100, left[i], right[i])
-      times[i] <- median(x)
+      times[i] <- median(runif(100, left[i], right[i]))
     } else if (impute == "harmonic_median") {
-      x <- runif(100, left[i], right[i])
-      times[i] <- 1 / median(1 / x)
+      x <- runif(100, left[i], right[i]); times[i] <- 1 / median(1 / x)
     } else if (impute == "geometric_median") {
-      x <- runif(100, left[i], right[i])
-      times[i] <- exp(median(log(x)))
+      x <- runif(100, left[i], right[i]); times[i] <- exp(median(log(x)))
     } else if (impute == "random_survival") {
       S_L <- 1 - cdf_fn(left[i])
       S_R <- 1 - cdf_fn(right[i])
@@ -121,40 +124,11 @@ mle_imp <- function(left, right, dist = "weibull",
       }
     }
   }
+
   times <- pmax(times, 1e-5)
 
   # --- Final MLE on imputed data
-  neg_loglik_final <- switch(dist,
-    "weibull" = function(par) -sum(dweibull(times, par[1], par[2], log = TRUE)),
-    "lognormal" = function(par) -sum(dlnorm(times, par[1], par[2], log = TRUE)),
-    "loglogistic" = function(par) {
-      shape <- par[1]; scale <- par[2]
-      logf <- log(shape) - log(scale) + (shape - 1) * log(times / scale) -
-              2 * log1p((times / scale)^shape)
-      -sum(logf)
-    },
-    "logistic" = function(par) -sum(dlogis(times, par[1], par[2], log = TRUE)),
-    "normal" = function(par) -sum(dnorm(times, par[1], par[2], log = TRUE)),
-    "gamma" = function(par) -sum(dgamma(times, par[1], par[2], log = TRUE)),
-    "gompertz" = function(par) {
-      a <- par[1]; b <- par[2]
-      if (a <= 0 || b <= 0) return(Inf)
-      logf <- log(b) + a * times - (b / a) * (exp(a * times) - 1)
-      -sum(logf)
-    },
-    "EMV" = function(par) {
-      loc <- par[1]; scale <- par[2]
-      if (scale <= 0 || any(times <= 0)) return(Inf)
-      z <- (log(times) - loc) / scale
-      logf <- -log(times) - log(scale) - z - exp(-z)
-      -sum(logf)
-    },
-    "exp" = function(par) {
-      if (par[1] <= 0) return(Inf)
-      -sum(dexp(times, rate = 1 / par[1], log = TRUE))
-    }
-  )
-
+  neg_loglik_final <- neg_loglik_init  # reuse the same function
   fit <- optim(par = est_par, fn = neg_loglik_final, method = "L-BFGS-B",
                lower = rep(1e-5, length(est_par)), hessian = TRUE)
 
@@ -166,19 +140,9 @@ mle_imp <- function(left, right, dist = "weibull",
     tvals <- estimates / se
     pvals_raw <- 2 * (1 - pnorm(abs(tvals)))
     pvals <- ifelse(pvals_raw < 2e-16, "<2e-16", formatC(pvals_raw, digits = 6, format = "f"))
-    results <- data.frame(
-      Estimate = estimates,
-      Std.Error = se,
-      t.value = tvals,
-      p.value = pvals
-    )
+    results <- data.frame(Estimate = estimates, Std.Error = se, t.value = tvals, p.value = pvals)
   } else {
-    results <- data.frame(
-      Estimate = estimates,
-      Std.Error = NA,
-      t.value = NA,
-      p.value = NA
-    )
+    results <- data.frame(Estimate = estimates, Std.Error = NA, t.value = NA, p.value = NA)
   }
 
   return(list(
